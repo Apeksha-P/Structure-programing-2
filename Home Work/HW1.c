acb7f55ac3d65bec344df7e1bf0ecd3b7ef13871
Apeksha60 <pavithriapeksha1new@gmail.com>
2023-06-22 23:11:08 +0530

    Try

--- Home Work/HW1.c
+++ Home Work/HW1.c
@@ -3,18 +3,45 @@
 #include <stdlib.h>
 int main(){
     char arr[20];
-    int x,i,r;
+
+    int x,i;
+    int r=0;
     printf("Enter Character values :\n");
     scanf("%s",&arr);
     x=strlen(arr);
-    for(i=1;i<x;i++){
-        if(arr[i]==arr[x-i+1]){
+    char rev[x];
+    int value;
+    for(i=0;i<x;i++){
+        rev[i] = arr[x-i+1];
+        /*if(arr[i]==arr[x-i+1]){
             r=1;
         }
         else{
             r=0;
-        }
+        }*/
     }
-    printf("%s",arr);
+    
+    /*for(i=0;i<x;i++){
+        if(arr[i]==rev[i]){
+            r += 1;
+        }
+        else{
+            printf("no");
+            break;
+        }
+    }*/
+    value = strcmp(arr, rev);  
+    if(value == 0)  
+        printf("strings are same");  
+    else  
+        printf("strings are not same");
+    /*if(rev==arr){
+        printf("yes");
+    }
+    else{
+        printf("no");
+    }*/
+    /*printf("%s",rev);
+    printf("%s",arr);*/
     return 0;
 }

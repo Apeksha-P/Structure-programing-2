a4c473d2af663c9821391bc66b6036b4f4cdb981
H.P.P. Apeksha <118092627+Apeksha60@users.noreply.github.com>
2023-08-01 11:20:49 +0530

    Passing string value

---
+++ learning/string.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+void display(char []);
+int main(){
+    char str[]="Apeksha";
+    display(str);
+    return 0;
+}
+void display(char str[]){
+    printf("string is %s",str);
+}

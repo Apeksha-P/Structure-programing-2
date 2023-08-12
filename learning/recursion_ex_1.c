14fe1ae16f61d6ac1777d91e90c27f555179a529
H.P.P. Apeksha <118092627+Apeksha60@users.noreply.github.com>
2023-08-12 14:49:30 +0530

    Recursion Exercise 1:

---
+++ learning/recursion_ex_1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void display(int n){
+    if(n<1)
+        return;
+    else{
+        printf("%d",n);
+        display(n-1);
+        printf("%d",n);
+    }
+}
+void main(){
+    int n;
+    printf("Enter value to n: ");
+    scanf("%d",&n);
+    display(n);
+}

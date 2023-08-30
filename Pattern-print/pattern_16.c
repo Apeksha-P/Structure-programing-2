1140c04befedba725db917421edd1562bd00f114
H.P.P. Apeksha <118092627+Apeksha60@users.noreply.github.com>
2023-08-30 08:57:52 +0530

    Create pattern 16
    5
    A
    B B
    C C C
    D D D D
    E E E E E

---
+++ Pattern-print/pattern_16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    int y = 65;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+
+        for(int j=1;j<=i;j++){
+            printf("%c ",y);
+        }
+        y++;
+        printf("\n");
+    }
+    return 0;
+}
